59066cd39974aa19ef2256d7dc32555d0e473264
Jess Taylor <jess.taylor@sainsburys.co.uk>
2020-09-19 02:06:38 +0100

    Update Navbar.js
    Added navbar collapse to links in the nav

--- src/components/Nav/Navbar.js
+++ src/components/Nav/Navbar.js
@@ -20,11 +20,11 @@ const Navbar = () => {
             </button>
             <nav className={styles.toplinks}>
                 <ul id="topLinks" className={styles.toplinksItems}>
-                    <li><Link to={'/'}>Home</Link><span className={styles.spacer}>|</span></li>
-                    <li><AnchorLink href="#about">About Me</AnchorLink><span className={styles.spacer}>|</span></li>
-                    <li><AnchorLink href="#portfolio">My Work</AnchorLink><span className={styles.spacer}>|</span></li>
+                    <li><Link onClick={handleNavCollapse} to={'/'}>Home</Link><span className={styles.spacer}>|</span></li>
+                    <li><AnchorLink onClick={handleNavCollapse} href="#about">About Me</AnchorLink><span className={styles.spacer}>|</span></li>
+                    <li><AnchorLink onClick={handleNavCollapse} href="#portfolio">My Work</AnchorLink><span className={styles.spacer}>|</span></li>
                     {/* <li><Link to={'/blog'}>Blog</Link><span className={styles.spacer}>|</span></li> */}
-                    <li><AnchorLink href="#contact">Contact</AnchorLink></li>
+                    <li><AnchorLink onClick={handleNavCollapse} href="#contact">Contact</AnchorLink></li>
                 </ul>
             </nav>
         </div>
