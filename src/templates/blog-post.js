2f23432ed72962608fd3494e0ec4baed678e57f0
Jess Taylor <jess.taylor@sainsburys.co.uk>
2020-09-21 13:36:28 +0100

    Update blog-post.js
    Fixed blog posts using the wrong layout

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 
 import Bio from "../components/bio"
-import Layout from "../components/layout"
+import BlogLayout from "../components/layout"
 import SEO from "../components/seo"
 
 const BlogPostTemplate = ({ data, pageContext, location }) => {
@@ -11,7 +11,7 @@ const BlogPostTemplate = ({ data, pageContext, location }) => {
   const { previous, next } = pageContext
 
   return (
-    <Layout location={location} title={siteTitle}>
+    <BlogLayout location={location} title={siteTitle}>
       <SEO
         title={post.frontmatter.title}
         description={post.excerpt}
@@ -62,7 +62,7 @@ const BlogPostTemplate = ({ data, pageContext, location }) => {
           </li>
         </ul>
       </nav>
-    </Layout>
+    </BlogLayout>
   )
 }
 
