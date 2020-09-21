545360ebfef8945d9ad1aa01f60f7bb1bdf3163a
Jess Taylor <jess.taylor@sainsburys.co.uk>
2020-09-21 13:46:21 +0100

    Fixed blog posts getting wrong header

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 
 import Bio from "../components/bio"
-import BlogLayout from "../components/layout"
+import BlogLayout from "../components/layout-blog"
 import SEO from "../components/seo"
 
 const BlogPostTemplate = ({ data, pageContext, location }) => {
